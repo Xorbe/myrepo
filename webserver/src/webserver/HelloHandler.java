7d70e27527cad4b3eec2323535f411ea8d304872
Stef Janssens <stef.janssens@xorgaming.net>
2015-12-17 22:59:31 +0100

    add Jetty library and basic server code

---
+++ webserver/src/webserver/HelloHandler.java
@@ -0,0 +1,24 @@
+package webserver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class HelloHandler extends AbstractHandler
+{
+
+	@Override
+	public void handle(String arg0, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+	}
+	
+}
